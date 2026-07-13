d54456cc3ccd08ad46bb0b2baa2bc7df487b1023
Jonathan.Manucom <Jonathan.Manucom@gmail.com>
2026-07-14 04:07:40 +1200

    Implemented Task REST API

---
+++ backend/tasks/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+    ]
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='TODO'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+
+    def __str__(self):
+        return self.title
